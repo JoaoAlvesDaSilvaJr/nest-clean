8979dc2b5854c10c7ac870153997a752db6e5719
Joao Alves <kanonxp@gmail.com>
2025-03-20 18:58:37 -0300

    fix: correcting prism module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+
 import { ConfigModule } from '@nestjs/config';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 import { CreateAccountController } from './controllers/create-accounts.controller';
 import { envSchema } from './env';
 
@@ -10,8 +11,9 @@ import { envSchema } from './env';
       isGlobal: true,
       validate: (config) => envSchema.parse(config),
     }),
+    PrismaModule,
   ],
   controllers: [CreateAccountController],
-  providers: [PrismaService],
+  providers: [],
 })
 export class AppModule {}
