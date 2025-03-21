8118964a1c7dc193d1a82e8625a1f006403679ae
Joao Alves <kanonxp@gmail.com>
2025-03-21 19:03:27 -0300

    fix: Lint disabled in jwt.strategy.ts

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
@@ -14,15 +15,18 @@ type TokenSchema = z.infer<typeof tokenSchema>;
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(config: ConfigService<Env, true>) {
-    const publickey = config.get('JWT_PUBLIC_KEY', { infer: true });
+    const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
 
     super({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: Buffer.from(publickey, 'base64'),
+      secretOrKey: Buffer.from(publicKey, 'base64'),
       algorithms: ['RS256'],
     });
   }
-  async validate(payload: TokenSchema) {
+
+  validate(payload: TokenSchema) {
+    //original com  async
     return tokenSchema.parse(payload);
   }
 }
--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   JWT_PRIVATE_KEY: z.string(),
-  JWT_PUBLIC_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string().optional().default(''),
   PORT: z.coerce.number().optional().default(3333),
 });
 
