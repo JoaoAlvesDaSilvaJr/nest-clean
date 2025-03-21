2564700eb0a24c8586397c6b926905f088edf06e
Joao Alves <kanonxp@gmail.com>
2025-03-21 14:19:56 -0300

    feat: jwt authentication configuration created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CreateAccountController } from './controllers/create-accounts.controller';
 import { envSchema } from './env';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { envSchema } from './env';
       validate: (config) => envSchema.parse(config),
     }),
     PrismaModule,
+    AuthModule,
   ],
   controllers: [CreateAccountController],
   providers: [],
